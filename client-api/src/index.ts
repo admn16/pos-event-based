bb4a84bb9881c5ea08aeb537d70c5d4bc7a2b6f3
Anton Nocum <anton.nocum@gmail.com>
2021-11-08 13:13:03 +0800

    Added sample kafka code

--- client-api/src/index.ts
+++ client-api/src/index.ts
@@ -1,13 +1,39 @@
 import express from "express";
+import { Kafka, logLevel } from "kafkajs";
 
-const PORT =  process.env.PORT || 4000;
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["kafka:9092"],
+  logLevel: logLevel.ERROR,
+});
+
+const producer = kafka.producer();
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.post("/order", async ({ body }, res) => {
+  await producer.connect();
+  await producer.send({
+    topic: "order-created",
+    messages: [
+      {
+        value: JSON.stringify(body),
+      },
+    ],
+  });
+
   res.send({
-    test: 'Client API'
-  })
-})
+    success: true,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    test: "Client API",
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- core/src/index.ts
+++ core/src/index.ts
@@ -1,13 +1,21 @@
+import 'reflect-metadata'
 import express from "express";
+import { Kafka, logLevel } from "kafkajs";
 
-const PORT =  process.env.PORT || 4000;
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["kafka:9092"],
+  logLevel: logLevel.ERROR,
+});
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", async (req, res) => {
   res.send({
-    test: 'CORE API'
-  })
-})
+    test: "CORE API",
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
